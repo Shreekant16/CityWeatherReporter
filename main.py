785dfbd5257ecfc3eb01db28ba89772856feb520
Shreekant Suhas Pukale <111984571+Shreekant16@users.noreply.github.com>
2022-10-05 11:11:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from tkinter import *
+
+window = Tk()
+window.geometry("300x200")
+label = Label(text="City : ")
+label.grid(column=0, row=0)
+entry = Entry()
+entry.grid(column=1, row=0)
+
+
+def home():
+    city = entry.get()
+    url = f"https://weather-request.p.rapidapi.com/weather/{city}"
+    headers = {
+        "X-RapidAPI-Key": "f2b1123709msh2a86b29c16fae58p10b040jsn7d22ba3230b8",
+        "X-RapidAPI-Host": "weather-request.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers)
+    data = response.json()[0]
+    weather = data['weather']
+    temperatue = data['temperature']
+    lebel1.config(text=f"{city}\n"
+                       f"Weather : {weather}\n"
+                       f"Temperature : {temperatue}\n")
+
+
+button = Button(text="Submit", command=home)
+button.grid(column=1, row=1)
+lebel1 = Label(text="XXXx")
+lebel1.grid(column=1,row=2)
+window.mainloop()
